9f121a9198f28c73bec879afb6d65cd7b5bc1b51
MicaSC98 <micacarollo98@gmail.com>
2026-05-12 22:34:45 -0300

    Hello

--- componentes2/src/App.vue
+++ componentes2/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
+import Hello from './components/Hello.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import SumBoton from './components/SumBoton.vue'
-import Hello from './components/Hello.vue'
+
 </script>
 
 <template>
+  <Hello/>
   <SumBoton />
   <HelloWorld />
-  <Hello/>
+  
 </template>
--- componentes2/src/components/hello.vue
+++ componentes2/src/components/hello.vue
@@ -1,13 +1,6 @@
-<script setup>
-
-</script>
-
 <template>
     <button>
     Hello
     </button>
 </template>
 
-<style scoped>
-
-</style>
